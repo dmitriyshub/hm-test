9e984f1c5b27e0d2ce6ff540822b6de15227f7a1
dmitriyshub <d.shtranvasser@gmail.com>
2022-08-10 16:59:12 +0300

    adding is_odd_or_even, is_div_by_5 function and some comments

--- calc.sh
+++ calc.sh
@@ -11,12 +11,13 @@ sum=0
 #########FUNCTION#########FUNCTIONS#########FUNCTIONS#########
 ##############################################################
 
+##########################
 function is_prime() {
 sum=$1
 for((i=2; i<=$sum/2; i++))
 do
-  ans=$(( sum%i ))
-  if [ $ans -eq 0 ]
+  answer=$(( sum%i ))
+  if [ $answer -eq 0 ]
   then
     echo "$sum is not a prime number."
     exit
@@ -24,44 +25,62 @@ do
 done
 echo "$sum is a prime number."
 }
+##########################
+function is_odd_or_even() {
 
-
+    sum=$1
+    odd_even="null"
+    if [ $(( $sum % 2 )) -eq 0 ]
+    then odd_even="even"
+    else
+      odd_even="odd"
+      fi
+    echo $odd_even
+}
+##########################
+function is_div_by_5() {
+    if [ $(( $1 % 5 )) -ne 0 ]; then
+      echo The number cant be devided by 5
+    else
+      echo The number can be devivded by 5
+    fi
+}
+##########################
 function addition()
 {
     local sum=$(($1 + $2))
     echo $sum
 }
-
+##########################
 function substruct()
 {
   local sum=$(($1 - $2))
   echo $sum
 }
-
+##########################
 function multiplication() {
-  local sum=$(($1 * $2 ))
+  local sum=$(($1 * $2))
   echo $sum
 }
-
+##########################
 function divide()
 {
   local sum=$(($1 / $2))
   echo $sum
 }
-
+##########################
 function power_of()
 {
   local sum=$(($1 ** $2))
   echo $sum
 }
-
+##########################
 function modulo()
 {
   local sum=$(($1 % $2))
   echo $sum
 }
-
-
+##########################
 function menu()
 {
 echo "Enter Two numbers : "
@@ -93,6 +112,8 @@ case $choice in
   6)modulo $a $b
 esac
 }
+##########################
+
 
 #########MAIN#########MAIN#########MAIN#########
 menu
