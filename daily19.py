86b52fb6aaa6bb2503d91093a8907876f93faa91
dmitriyshub <d.shtranvasser@gmail.com>
2022-08-28 22:22:31 +0300

    Python19daily

---
+++ daily19.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+print("Hola Mundo")
+
+print(("#"*26 + "\n" + "+ Bash Scripting Miss U! +" + "\n") + "#"*26)
+
+last="Shtranvasser"
+
+print(f"Hello {last}")
+print("Hello", last)
+print("Hello %s" % (last))
+
+num = 77
+num += 5
+num -= 5
+print(num * 5)
+print(num * num)
+print(num / 3)
+print(num / 7)
+print(num // 7)
+print(num % 3)
+
+num2 = int(input("please give me a number: "))
+print(num2*3)
+
+x, y = 10, 20
+
+print(f"x = {x} y = {y}")
+
+print("x * y =", x * y)
+
+print(f"x / y = {x/y}")
