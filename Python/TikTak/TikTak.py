6181e07096f2b9c453a9aa339c500c1790df11c4
dmitriyshub <d.shtranvasser@gmail.com>
2022-08-31 19:20:51 +0300

    remove return from if conditions

--- Python/TikTak/TikTak.py
+++ Python/TikTak/TikTak.py
@@ -31,20 +31,17 @@ def check_win(play):
     if play > 5 :
         if (spot[1] == spot[2] == spot[3]) or (spot[4] == spot[5] == spot[6]) or (spot[7] == spot[8] == spot[9]) :
             print(" *** !won! *** ")
-            return True
         elif (spot[1] == spot[4] == spot[7]) or (spot[2] == spot[5] == spot[8]) or (spot[3] == spot[6] == spot[9]):
             print(" *** !won! *** ")
-            return True
         elif (spot[1] == spot[5] == spot[9]) or (spot[3] == spot[5] == spot[7]):  # across the top
             print(" *** !won! *** ")
-            return True
 
         elif play == 10:
-            print("DRAW")
+            print("DRAW!")
             exit()
         else:
             return False
-
+        return True
 
 ######MAIN#######
 print('Enjoy! press "exit" to stop!')
