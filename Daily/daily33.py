38a9b53c4235c70f2366dff9d924a7369e55617e
dmitriyshub <d.shtranvasser@gmail.com>
2022-09-20 22:57:40 +0300

    Final dailt33

--- Daily/daily33.py
+++ Daily/daily33.py
@@ -8,3 +8,62 @@ Dictionary = {
 with open('file2.json', 'w') as jsonfile:
 
     json_dict = json.dump(Dictionary,jsonfile)
+
+'''
+SELECT YEAR(`orderDate`) AS `Year`, COUNT(`status`) AS `TotalOrders` FROM `orders` GROUP BY `year`;
+SELECT SUM(`amount`) AS `total`, YEAR(`orderDate`) AS `year` FROM `payments` INNER JOIN `orders` USING (`customerNumber`) GROUP BY year
+'''
+'''
+======================================================================
+AWS:
+1. Using Amazon RDS falls under the shared responsibility model. Which of the
+following are customer responsibilities? (Choose TWO)
+
+Building the relational database schema << YES
+Patching the database software
+Managing the database settings << YES
+Performing backups
+Installing the database
+
+2. Under the Shared Responsibility Model, which of the following controls do
+customers fully inherit from AWS? (Choose TWO)
+
+Physical controls << YES
+Awareness & training
+Database controls
+Patch management controls
+Environmental controls << YES
+
+3. What are AWS shared controls?
+
+Controls that the customer and AWS collaborate together upon the secure
+the infrastructure
+
+Controls that a customer inherits from AWS
+
+Controls that are solely the responsibility of the customer based on the
+application they are deploying within AWS services
+
+Controls that apply to both the infrastructure layer and customer layers << YES
+
+4. According to the AWS shared responsibility model, what are the controls that
+customers fully inherit from AWS? (Choose TWO)
+
+Awareness and Training
+Data center security controls << YES
+Environmental controls << YES
+Resource Configuration Management
+Communications controls
+
+5. Which of the following is NOT correct regarding Amazon EC2 On-demand
+instances?
+
+With on-demand instances, no longer-term commitments or upfront payments
+are needed
+You have to pay a start-up fee when launching a new instance for the first time << YES
+When using on-demand Linux instances, you are charged per second based on
+an hourly rate
+The on-demand instances follow the AWS pay-as-you-go pricing model
+
+======================================================================
+'''
--- Daily/daily33.sh
+++ Daily/daily33.sh
@@ -64,57 +64,13 @@ is_palindrome(){
 
 is_palindrome level
 
+
 '''
-======================================================================
-AWS:
-1. Using Amazon RDS falls under the shared responsibility model. Which of the
-following are customer responsibilities? (Choose TWO)
 
-Building the relational database schema << YES
-Patching the database software
-Managing the database settings
-Performing backups
-Installing the database
+aws iam list-user-policies --user-name awsstudent
+aws iam list-policies | grep "lab policy"
+aws iam get-policy --policy-arn arn:aws:iam::************:policy/lab_policy
+aws iam get-policy-version --policy-arn arn:aws:iam::925870290403:policy/lab_policy --version-id v1 > lab_policy.json
 
-2. Under the Shared Responsibility Model, which of the following controls do
-customers fully inherit from AWS? (Choose TWO)
 
-Physical controls << YES
-Awareness & training
-Database controls
-Patch management controls
-Environmental controls << YES
-
-3. What are AWS shared controls?
-
-Controls that the customer and AWS collaborate together upon the secure
-the infrastructure
-
-Controls that a customer inherits from AWS
-
-Controls that are solely the responsibility of the customer based on the
-application they are deploying within AWS services
-
-Controls that apply to both the infrastructure layer and customer layers << YES
-
-4. According to the AWS shared responsibility model, what are the controls that
-customers fully inherit from AWS? (Choose TWO)
-
-Awareness and Training
-Data center security controls << YES
-Environmental controls << YES
-Resource Configuration Management
-Communications controls
-
-5. Which of the following is NOT correct regarding Amazon EC2 On-demand
-instances?
-
-With on-demand instances, no longer-term commitments or upfront payments
-are needed
-You have to pay a start-up fee when launching a new instance for the first time << YES
-When using on-demand Linux instances, you are charged per second based on
-an hourly rate
-The on-demand instances follow the AWS pay-as-you-go pricing model
-
-======================================================================
 '''
