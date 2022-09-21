d1981111e939b565ff50e96773e36c2d3ce65644
dmitriyshub <d.shtranvasser@gmail.com>
2022-09-22 00:23:40 +0300

    sdk with dynamodb example

---
+++ Lecture.py
@@ -0,0 +1,45 @@
+import boto3
+
+aws_access_key_id="*************"
+aws_secret_access_key="****************************"
+aws_session_token="*****************************************************************"
+def create_table():
+    session = boto3.Session(
+        region_name="us-west-2",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token
+    )
+
+    dynamodb = session.resource('dynamodb')
+
+
+    table = dynamodb.create_table(
+        TableName='Users',
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            },
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'N'
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1,
+        }
+    )
+
+    print("Table status:", table.table_status)
+
+    # Wait until the table exists.
+    table.wait_until_exists()
+    # Print out some data about the table.
+    print(table.item_count)
+
+if __name__ == '__main__':
+    create_table()
