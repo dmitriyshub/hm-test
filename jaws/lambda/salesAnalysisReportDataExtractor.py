ac7d6dac13747a228645d11ba6899e6c51aff244
dmitriyshub <d.shtranvasser@gmail.com>
2022-09-27 20:12:32 +0300

    lambda lab

---
+++ jaws/lambda/salesAnalysisReportDataExtractor.py
@@ -0,0 +1,44 @@
+import boto3
+import pymysql
+import sys
+
+def lambda_handler(event, context):
+
+    # Retrieve the database connection information from the event input parameter.
+
+    dbUrl = event['dbUrl']
+    dbName = event['dbName']
+    dbUser = event['dbUser']
+    dbPassword = event['dbPassword']
+
+    # Establish a connection to the Cafe database, and set the cursor to return results as a Python dictionary.
+
+    try:
+        conn = pymysql.connect(host=dbUrl, user=dbUser, passwd=dbPassword, db=dbName, cursorclass=pymysql.cursors.DictCursor)
+
+    except pymysql.Error as e:
+        print('ERROR: Failed to connect to the Cafe database.')
+        print('Error Details: %d %s' % (e.args[0], e.args[1]))
+        sys.exit()
+
+    # Execute the query to generate the daily sales analysis result set.
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT  c.product_group_number, c.product_group_name, a.product_id, b.product_name, CAST(sum(a.quantity) AS int) as quantity FROM order_item a, product b, product_group c WHERE b.id = a.product_id AND c.product_group_number = b.product_group GROUP BY c.product_group_number, a.product_id")
+        result = cur.fetchall()
+
+    # Close the connection.
+
+    conn.close()
+
+    # Return the result set.
+
+    return {'statusCode': 200, 'body': result}
+'''
+{
+  "dbUrl": "ec2-35-91-43-128.us-west-2.compute.amazonaws.com",
+  "dbName": "cafe_db",
+  "dbUser": "root",
+  "dbPassword": "Re:Start!9"
+}
+'''
