2239bc3003fbde20617254fe5ab65c1d896f6cad
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-20 08:29:22 +0300

    truncate

--- x.py
+++ x.py
@@ -9,3 +9,7 @@ numbers = [rd.randint(1,150) for i in range(50)]
 print(numbers)
 above_120 = [n * 1.2 for n in numbers if n > 120]
 print(above_120)
+
+def tr(lst):
+    out = [[lst[j][i] for j in range(len(lst))] for i in range(len(lst[0]))]
+    return out
