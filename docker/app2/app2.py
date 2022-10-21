a9b0b3f9139d8bbb2bae4a09ae821f2c3c25f49f
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-21 13:13:11 +0300

    app2

---
+++ docker/app2/app2.py
@@ -0,0 +1,11 @@
+from flask import Flask, requests
+import json
+
+app2 = Flask(__name__)
+
+@app2.route('/')
+def hello_world():
+    return 'Hello World from app 2!'
+
+if __name__ == '__main__':
+    app2.run(debug=True, host='0.0.0.0')
